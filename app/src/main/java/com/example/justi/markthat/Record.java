3697d5822ad77263455fa450234e58e4c32b6da4
jstnzl <jstnzliu@gmail.com>
2018-10-30 11:25:39 -0700

    Base record code

--- app/src/main/java/com/example/justi/markthat/Record.java
+++ app/src/main/java/com/example/justi/markthat/Record.java
@@ -1,7 +1,14 @@
 package com.example.justi.markthat;
 
+import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.media.MediaRecorder;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Record extends AppCompatActivity {
 
@@ -9,5 +16,40 @@ public class Record extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        public void toggleRecord(String fileName) {
+            final MediaRecorder recorder = new MediaRecorder();
+            ContentValues values = new ContentValues(3);
+            values.put(MediaStore.MediaColumns.TITLE, fileName);
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+            recorder.setOutputFile("/sdcard/sound/" + fileName);
+            try {
+                recorder.prepare();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+            final ProgressDialog mProgressDialog = new ProgressDialog(Record.this);
+            mProgressDialog.setTitle(R.string.lbl_recording);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgressDialog.setButton("Stop recording", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    mProgressDialog.dismiss();
+                    recorder.stop();
+                    recorder.release();
+                }
+            });
+
+            mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener(){
+                public void onCancel(DialogInterface p1) {
+                    recorder.stop();
+                    recorder.release();
+                }
+            });
+            recorder.start();
+            mProgressDialog.show();
+        }
     }
 }
