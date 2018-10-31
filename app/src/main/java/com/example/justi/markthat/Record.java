44324c1f624ca14b4f2856816afe62969bc0760f
jstnzl <jstnzliu@gmail.com>
2018-10-30 21:33:42 -0700

    Trying to get record to work

--- app/src/main/java/com/example/justi/markthat/Record.java
+++ app/src/main/java/com/example/justi/markthat/Record.java
@@ -1,55 +1,150 @@
 package com.example.justi.markthat;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Record extends AppCompatActivity {
+    private boolean recording = false;
+    private MediaRecorder recorder = null;
+    private static final String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
+    private static final String AUDIO_RECORDER_FILE_EXT_MP4 = ".mp4";
+    private static final String AUDIO_RECORDER_FOLDER = "MarkThat";
+    private int currentFormat = 0;
+    private int output_formats[] = { MediaRecorder.OutputFormat.MPEG_4, MediaRecorder.OutputFormat.THREE_GPP };
+    private String file_exts[] = { AUDIO_RECORDER_FILE_EXT_MP4, AUDIO_RECORDER_FILE_EXT_3GP };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
 
-        public void toggleRecord(String fileName) {
-            final MediaRecorder recorder = new MediaRecorder();
-            ContentValues values = new ContentValues(3);
-            values.put(MediaStore.MediaColumns.TITLE, fileName);
-            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-            recorder.setOutputFile("/sdcard/sound/" + fileName);
-            try {
-                recorder.prepare();
-            } catch (Exception e){
-                e.printStackTrace();
+        final FloatingActionButton recordButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        recordButton.setOnClickListener(buttonListeners);
+    }
+
+    private View.OnClickListener buttonListeners = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId() /*to get clicked view id**/) {
+                case R.id.floatingActionButton:
+                    if(recording){
+                        if(null != recorder) {
+                            recording = false;
+                            recorder.stop();
+                            recorder.reset();
+                            recorder.release();
+                            recorder = null;
+                        }
+                    }
+                    else {
+                        requestAudioPermissions();
+                        recording = true;
+                    }
+                    break;
+                case R.id.floatingActionButton3:
+
+                    break;
+                default:
+                    break;
             }
+        }
+    };
 
-            final ProgressDialog mProgressDialog = new ProgressDialog(Record.this);
-            mProgressDialog.setTitle(R.string.lbl_recording);
-            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            mProgressDialog.setButton("Stop recording", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                    mProgressDialog.dismiss();
-                    recorder.stop();
-                    recorder.release();
-                }
-            });
-
-            mProgressDialog.setOnCancelListener(new DialogInterface.OnCancelListener(){
-                public void onCancel(DialogInterface p1) {
-                    recorder.stop();
-                    recorder.release();
-                }
-            });
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(output_formats[currentFormat]);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(getFilename());
+        try {
+            recorder.prepare();
             recorder.start();
-            mProgressDialog.show();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getFilename() {
+//        File root = new File(Environment.getExternalStorageDirectory(), AUDIO_RECORDER_FOLDER);
+//        if (!root.exists()) {
+//            root.mkdirs();
+//        }
+        return (Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + System.currentTimeMillis() + file_exts[currentFormat]);
+    }
+
+    //Requesting run-time permissions
+
+    //Create placeholder for user's consent to record_audio permission.
+    //This will be used in handling callback
+    private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
+
+    private void requestAudioPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            //When permission is not granted by user, show them message why this permission is needed.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                Toast.makeText(this, "Please grant permissions to record audio", Toast.LENGTH_LONG).show();
+
+                //Give user option to still opt-in the permissions
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+
+            } else {
+                // Show user dialog to grant permission to record audio
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+            }
+        }
+        //If permission is granted, then go ahead recording audio
+        else if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            //Go ahead with recording audio now
+            startRecording();
+        }
+    }
+
+    //Handling callback
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_RECORD_AUDIO: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay!
+                    startRecording();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(this, "Permissions Denied to record audio", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
         }
     }
 }
