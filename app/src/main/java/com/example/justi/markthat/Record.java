66fdda3a821f37e5030f187645e69289c479b356
Gregh <greghan9@gmail.com>
2018-10-31 20:02:05 -0700

    Added titles to Record and ViewRec pages. Added back button on Record that returns to Home

--- app/src/main/java/com/example/justi/markthat/Record.java
+++ app/src/main/java/com/example/justi/markthat/Record.java
@@ -1,7 +1,10 @@
 package com.example.justi.markthat;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Record extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class Record extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+        //title of page
+        String recordTitle = ("MarkThat - Record");
+        //set up back button
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(recordTitle);
+        }
+        //alt method that dont work
+        //ActionBar actionBar = getActionBar();
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+        //change title of page
+       // setTitle(recordTitle);
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item){ //back button
+        Intent myIntent = new Intent(getApplicationContext(), Home.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+
     }
 }
