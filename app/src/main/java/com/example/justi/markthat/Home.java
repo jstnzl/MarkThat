45e5865ebd325a5f79e26cb0bb14f73a67982717
Justin Wu <jwu90@ucsc.edu>
2018-11-03 16:36:38 -0700

    home layout constraints + added listview for recent recordings

--- app/src/main/java/com/example/justi/markthat/Home.java
+++ app/src/main/java/com/example/justi/markthat/Home.java
@@ -5,13 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
     }
+
+
+
 
     public void goTorecord(View view) {
         Intent intent = new Intent(this, Record.class);
