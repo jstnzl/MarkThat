445035459f50517e73e9f2620213cc04ef2e25df
greghan9 <44275620+greghan9@users.noreply.github.com>
2018-11-04 17:06:07 -0800

    Deleted back button and changed Home title. Fixed bad merge (#8)
    * Added titles to Record and ViewRec pages. Added back button on Record that returns to Home
    
    * Set title for home page. Used alternate method for Record back button
    
    * Deleted back button and changed Home title. Fixed bad merge
    
    * Second PR for record_greg2 cause fucked up first one and didnt open another branch for the fix

--- app/src/main/java/com/example/justi/markthat/Record.java
+++ app/src/main/java/com/example/justi/markthat/Record.java
@@ -1,7 +1,10 @@
 package com.example.justi.markthat;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class Record extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class Record extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+        //title
+        setTitle("MarkThat - Record");
+    }
+    public void hideKeyboard(View view){
+        InputMethodManager inputMethodManager=(InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 }
